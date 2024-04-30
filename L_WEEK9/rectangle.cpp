a45d9fe83da8c368121daff918d07c0d7b06ab47
FrancesscaWY <3449997118@qq.com>
2024-04-30 12:45:18 +0800

    三维数组的赋值运算

--- L_WEEK9/rectangle.cpp
+++ L_WEEK9/rectangle.cpp
@@ -1,11 +1,36 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main() {
-    int a, b, c;
-    cin >> a >> b >> c;
-    vector<vector<vector<int>>> t_box(a,vector<vector<int>>(b,vector<int>(c,1)));//定义一个三维数组,并初始化为1
-    int n;
-    cin >> n;
+const int N=25;
+int A[N][N][N];
+int a,b,c,d,e,f;
+int l,w,h;
+int n;
+int sum;
+void solve(){
+    for(int i=a;i<=d;i++){
+        for(int j=b;j<=e;j++){
+            for(int k=c;k<=f;k++){
+             A[i][j][k]=1;
+            }
+        }
+    }
+}
+int main(){
+    scanf("%d %d %d",&l,&w,&h);
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        scanf("%d %d %d %d %d %d",&a,&b,&c,&d,&e,&f);
+        solve();
+    }
+    for(int i=1;i<=l;i++){
+        for(int j=1;j<=w;j++){
+            for(int k=1;k<=h;k++){
+                if(A[i][j][k]==0){
+                    ++sum;
+                }
+            }
+        }
+    }
+    printf("%d\n",sum);
+    return 0;
 }
